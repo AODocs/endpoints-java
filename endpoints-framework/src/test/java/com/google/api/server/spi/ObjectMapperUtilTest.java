7410d450e0b98a98156c084a601ace1464161fbf
Clément Denis <clement@altirnao.com>
2019-11-25 14:23:19 +0100

    Improve Java 8 support in (de)serialization - Enable support for java.util.Optional (jackson-datatype-jdk8) - Enable support for parameter names for constructor detection on deserialization (jackson-module-parameter-names)

--- endpoints-framework/src/test/java/com/google/api/server/spi/ObjectMapperUtilTest.java
+++ endpoints-framework/src/test/java/com/google/api/server/spi/ObjectMapperUtilTest.java
@@ -17,15 +17,21 @@ package com.google.api.server.spi;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.api.server.spi.config.model.EndpointsFlag;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.server.spi.config.model.EndpointsFlag;
+import com.fasterxml.jackson.databind.ObjectReader;
 import org.junit.Test;
+
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Tests for {@link ObjectMapperUtil}
  */
 public class ObjectMapperUtilTest {
+  
   @Test
   public void createStandardObjectMapper_base64Variant() throws Exception {
     byte[] bytes = new byte[] {(byte) 0xff, (byte) 0xef};
@@ -51,6 +57,30 @@ public class ObjectMapperUtilTest {
     }
   }
 
+  @Test
+  public void createStandardObjectMapper_optional() throws Exception {
+    ObjectMapper mapper = ObjectMapperUtil.createStandardObjectMapper();
+    assertThat(mapper.writeValueAsString(new TestJava8Types(Optional.of("value")))).isEqualTo("{\"optional\":\"value\"}");
+    assertThat(mapper.writeValueAsString(new TestJava8Types(Optional.of("")))).isEqualTo("{\"optional\":\"\"}");
+    assertThat(mapper.writeValueAsString(new TestJava8Types(Optional.empty()))).isEqualTo("{\"optional\":null}");
+    assertThat(mapper.writeValueAsString(new TestJava8Types(null))).isEqualTo("{\"optional\":null}");
+
+    ObjectReader reader = mapper.readerFor(TestJava8Types.class);
+    assertThat(reader.<TestJava8Types>readValue("{\"optional\":\"value\"}").getOptional()).isEqualTo(Optional.of("value"));
+    assertThat(reader.<TestJava8Types>readValue("{\"optional\":\"\"}").getOptional()).isEqualTo(Optional.of(""));
+    assertThat(reader.<TestJava8Types>readValue("{\"optional\":null}").getOptional()).isEqualTo(Optional.empty());
+    assertThat(reader.<TestJava8Types>readValue("{}").getOptional()).isEqualTo(null);
+  }
+
+  @Test
+  public void createStandardObjectMapper_parameterNames() throws Exception {
+    ObjectMapper mapper = ObjectMapperUtil.createStandardObjectMapper();
+    TestParameterNames result = mapper.readerFor(TestParameterNames.class)
+        .readValue("{\"name\":\"Jerry\",\"surname\":\"Smith\"}");
+    assertThat(result).isEqualTo(new TestParameterNames("Jerry", "Smith"));
+  }
+
+
   private enum TestEnum {
     @JsonProperty("test") TEST
   }
@@ -70,4 +100,82 @@ public class ObjectMapperUtilTest {
       return test;
     }
   }
+  
+  private static class TestJava8Types {
+    Optional<String> optional;
+
+    public TestJava8Types() {
+    }
+
+    public TestJava8Types(Optional<String> optional) {
+      this.optional = optional;
+    }
+
+    public Optional<String> getOptional() {
+      return optional;
+    }
+
+    public void setOptional(Optional<String> optional) {
+      this.optional = optional;
+    }
+  }
+
+  private static class TestParameterNames {
+
+    private final String name;
+    private final String surname;
+
+    private String nickname;
+
+    //can't be deserialized if not build with -parameters compiler option
+    public TestParameterNames(String name, String surname) {
+      this.name = name;
+      this.surname = surname;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getSurname() {
+      return surname;
+    }
+
+    public String getNickname() {
+      return nickname;
+    }
+
+    public void setNickname(String nickname) {
+      this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      TestParameterNames that = (TestParameterNames) o;
+      return Objects.equals(name, that.name) &&
+          Objects.equals(surname, that.surname) &&
+          Objects.equals(nickname, that.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(name, surname, nickname);
+    }
+
+    @Override
+    public String toString() {
+      return "TestParameterNames{" +
+          "name='" + name + '\'' +
+          ", surname='" + surname + '\'' +
+          ", nickname='" + nickname + '\'' +
+          '}';
+    }
+  }
+  
 }
